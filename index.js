713eca501e59e3bbc9e3f3f462c04464580392e0
Shandukani19 <mudzwirisa@gmail.com>
2024-02-16 17:36:32 +0200

    improved the search engine on js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
 
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
