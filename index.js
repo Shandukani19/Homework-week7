06079b5633cc8aca26e451b4079fc494f3685177
Shandukani19 <mudzwirisa@gmail.com>
2024-02-16 20:28:42 +0200

    injected search functionality using API

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  
+}
+
+function searchCity(city) {
+  let apiKey = "t2a074aee318112cc56a6b83544bob8f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
 
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
+
+searchCity("Helsinki");
